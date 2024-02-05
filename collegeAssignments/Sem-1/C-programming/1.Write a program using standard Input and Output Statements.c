2a5a6b897e559c8bedee648d528f9ad30e84a5b7
5wapnilmore <mswapnil383@gmail.com>
2024-02-05 20:58:56 +0530

    Added c proramming file

---
+++ collegeAssignments/Sem-1/C-programming/1.Write a program using standard Input and Output Statements.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+    void main()
+    {
+        float x,y,ans;
+        printf("please enter first number : ");
+        scanf("%f",&x);
+        printf("please enter second number : ");
+        scanf("%f",&y);
+        
+        ans = x + y ;
+        
+        printf("the addition of %f and %f is %f",x,y,ans);
+        
+    }
