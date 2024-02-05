5b66016472a4c73026c91aa308646960bb040772
5wapnilmore <mswapnil383@gmail.com>
2024-02-05 21:38:51 +0530

    Added second c program

---
+++ collegeAssignments/Sem-1/C-programming/2. Write a program using formatted input output statements also study various format String and Escape sequence characters.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("*\t\t*********\n");
+    printf("*\t\t*\n");
+    printf("*\t\t*\n");
+    printf("*\t\t*\n");
+    printf("*****************\n");
+    printf("\t\t*\t\t*\n");
+    printf("\t\t*\t\t*\n");
+    printf("\t\t*\t\t*\n");
+    printf("*********\t\t*\n");
+
+    /*
+        // print Swastika Sign using user defined charactor
+
+        char x;
+        printf("type any charactar : ");
+        scanf("%c",&x);
+        printf("%c\t\t%c%c%c%c%c%c%c%c%c\n",x,x,x,x,x,x,x,x,x,x);
+        printf("%c\t\t%c\n",x,x);
+        printf("%c\t\t%c\n",x,x);
+        printf("%c\t\t%c\n",x,x);
+        printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x);
+        printf("\t\t%c\t\t%c\n",x,x);
+        printf("\t\t%c\t\t%c\n",x,x);
+        printf("\t\t%c\t\t%c\n",x,x);
+        printf("%c%c%c%c%c%c%c%c%c\t\t%c\n",x,x,x,x,x,x,x,x,x,x);
+    */
+}
