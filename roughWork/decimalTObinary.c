44231e3c846d45c154a3fa3e30aff3c37763d7ff
Swapnil More <157973024+5wapnilmore@users.noreply.github.com>
2024-02-07 12:35:07 +0530

    Update decimalTObinary.c
    added limit variable to execute loop 8 times

--- roughWork/decimalTObinary.c
+++ roughWork/decimalTObinary.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
+
 int main()
 {
-    binary(78);
+	binary(98);
 }
 
 int binary(int x)
 {
-    for(int i = 128; i>=0 && x>0; i = i/2)
+    int limit = 1;
+    for(int i = 128; i>=0 && limit<=8  ; i = i/2)
     {
       if(x>=i && x-i >= 0)
       {
@@ -18,5 +20,6 @@ int binary(int x)
       {
         printf("0");
       }
+      limit++;
     }
 }
