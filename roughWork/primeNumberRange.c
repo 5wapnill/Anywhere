3a1b70dc1689e2feb1050a244790d624125c3619
Swapnil More <157973024+5wapnilmore@users.noreply.github.com>
2024-02-21 21:49:11 +0530

    now parfected the code

--- roughWork/primeNumberRange.c
+++ roughWork/primeNumberRange.c
@@ -9,14 +9,14 @@ void main()
     for(int i = 1; i <= lvl; i++)
     {
         prime = 0;
-        for(int j = 2; j <= i-1; j++)
+        for(int j = 1; j <= i; j++)
         {
-            if (i % j == 0 && i != j)
+            if (i % j == 0 )
             {
-                prime = 1;
+                prime++;
             }
         }
-        if(prime == 0)
+        if(prime == 2)
         {
             printf("%d ",i);
         }
