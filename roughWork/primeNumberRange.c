a8de6513fc87daa7def9cac53233de7314be680f
5wapnilmore <mswapnil383@gmail.com>
2024-03-09 19:36:54 +0530

    modified prime number code

--- roughWork/primeNumberRange.c
+++ roughWork/primeNumberRange.c
@@ -2,23 +2,23 @@
 
 void main()
 {
-    int lvl,prime;
+    int lvl, prime;
     printf("please enter a range : ");
-    scanf("%d",&lvl);
-    
-    for(int i = 1; i <= lvl; i++)
+    scanf("%d", &lvl);
+
+    for (int i = 1; i <= lvl; i++)
     {
         prime = 0;
-        for(int j = 1; j <= i; j++)
+        for (int j = 1; j <= i; j++)
         {
-            if (i % j == 0 )
+            if (i % j == 0)
             {
                 prime++;
             }
         }
-        if(prime == 2)
+        if (prime == 2)
         {
-            printf("%d ",i);
+            printf("%d ", i);
         }
     }
 }
