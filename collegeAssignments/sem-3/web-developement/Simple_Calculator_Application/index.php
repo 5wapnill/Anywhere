394da2097f36956a1deb3f804352f095c09fa909
5wapnilmore <mswapnil383@gmail.com>
2024-08-16 13:38:40 +0530

    (done)

--- collegeAssignments/sem-3/web-developement/Simple_Calculator_Application/index.php
+++ collegeAssignments/sem-3/web-developement/Simple_Calculator_Application/index.php
@@ -3,25 +3,68 @@
 <head>
     <title>Document</title>
     <style>
-        .main {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-        .cal{
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            gap: 1em;
-        }
-        form{
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-            gap: 2em;
-        }
+
+body {
+    background-color: #111;
+    color: #fff;
+}
+
+.main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 50vh;
+}
+
+.cal {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1em;
+}
+
+form {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 2em;
+}
+
+.input {
+    height: 2.5em;
+    width: 12em;
+    border-radius: 5px;
+    padding: 0.5em;
+    text-align: center;
+    padding: 3px;
+    background-color: azure;
+    -moz-appearance: textfield;
+    outline: none;
+    font-size: 1.3em;
+
+}
+
+h1 {
+    text-align: center;
+    font-size: 3em;
+}
+
+.btn {
+    width: 8em;
+    height: 2.5em;
+    border-radius: 5px;
+    padding: 0.5em;
+    text-align: center;
+    padding: 3px;
+    background-color: azure;
+    font-size: 1.5em;
+    font-weight: 700;
+}
+
+select {
+    background-color: azure;
+}
 
     </style>
 
@@ -29,9 +72,9 @@
 <body>
 
     <div class="main">
-        <form action="index.php" class="form" target="_self">
+        <form action="index.php" class="form" method="GET" target="_self">
             <div class="cal">
-                <input type="number" name="num1" id="num2" class="input num1"></input>
+                <input type="number" name="num1" id="num2" class="input num1" placeholder="Enter first Number" value="<?php if(isset($_GET['num1'])) echo $_GET['num1'];else echo ""; ?>"></input>
                 <select name="operator" id="operator" class="input operator">
                     <option value="Select Operator">Select Operator</option>
                     <option value="+">+</option>
@@ -39,20 +82,23 @@
                     <option value="*">*</option>
                     <option value="/">/</option>
                 </select>
-                <input type="number" name="num2" id="num2" class="input num2"></input>
+                <input type="number" name="num2" id="num2" class="input num2" placeholder="Enter second Number" value="<?php if(isset($_GET['num2'])) echo $_GET['num2'];else echo ""; ?>"</input>
             </div>
             <button type="submit" class="btn">Calculate</button>
         </form>
     </div>
 
 </body>
+
 <?php
 
-$x = $_GET['num1'];
+if (isset($_GET['num1']) && isset($_GET['operator']) && isset($_GET['num2'])) {
+    
+$x = (int)$_GET['num1'];
 $operator = $_GET['operator'];
-$y = $_GET['num2'];
+$y = (int)$_GET['num2'];
 
-switch($operator){
+switch ($operator) {
     case "+":
         $ans = $x + $y;
         break;
@@ -69,7 +115,8 @@ switch($operator){
         $ans = "Select Operator";
         break;
 }
-echo $ans;
+echo "<h1> $x $operator $y = $ans </h1>";
+}
 
 ?>
 
