cadc15e2941a002e409ceb6685986fc13c76cb19
5wapnilmore <mswapnil383@gmail.com>
2024-08-18 21:49:04 +0530

    done

--- collegeAssignments/sem-3/web-developement/Number_Guessing_Game/code.php
+++ collegeAssignments/sem-3/web-developement/Number_Guessing_Game/code.php
@@ -1,29 +1,51 @@
+<style>
+
+*{
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+}
+body{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgb(0, 0, 0);
+    color: rgb(120, 120, 120);
+    padding: 50px;
+    font-size: 3em;
+    height: 80%;
+}
+span{
+    color: rgb(255, 225, 225);
+}
+
+</style>
+
 <?php
 
 $uNum = (int) $_POST['uNum'];
 $ranNum = (int) rand(0, 100);
 
 
-
 if (isset($uNum, $ranNum)) {
     if ($uNum == $ranNum) {
-        echo "you entered $uNum and the random number was $ranNum<br>congrats you guessed the number.";
+        echo "<div>you entered <span>$uNum</span> and the random number was <span>$ranNum</span><br>congrats you guessed the number.</div>";
     } else {
         if ($uNum > $ranNum) {
             if ($uNum - $ranNum < 10) {
-                echo "you entered $uNum and the random number was $ranNum<br>You are sooo close ! (within 10 of the number)";
+                echo "<div>you entered <span>$uNum</span> and the random number was <span>$ranNum</span><br>You are sooo close ! (within 10 of the number)</div>";
             } else if ($uNum - $ranNum < 20) {
-                echo "you entered $uNum and the random number was $ranNum<br>You are close (within 20 of the number.)";
+                echo "<div>you entered <span>$uNum</span> and the random number was <span>$ranNum</span><br>You are close (within 20 of the number.)</div>";
             } else {
-                echo "you entered $uNum and the random number was $ranNum<br>You are far from the number.";
+                echo "<div>you entered <span>$uNum</span> and the random number was <span>$ranNum</span><br>You are far from the number.<br> Better luck next time</div>";
             }
         } else if ($uNum < $ranNum) {
             if ($ranNum - $uNum < 10) {
-                echo "you entered $uNum and the random number was $ranNum<br>You are sooo close ! (within 10 of the number)";
+                echo "<div>you entered <span>$uNum</span> and the random number was <span>$ranNum</span><br>You are sooo close ! (within 10 of the number)</div>";
             } else if ($ranNum - $uNum < 20) {
-                echo "you entered $uNum and the random number was $ranNum<br>You are close (within 20 of the number.)";
+                echo "<div>you entered <span>$uNum</span> and the random number was <span>$ranNum</span><br>You are close (within 20 of the number.)</div>";
             } else {
-                echo "you entered $uNum and the random number was $ranNum<br>You are far from the number.";
+                echo "<div>you entered <span>$uNum</span> and the random number was <span>$ranNum</span><br>You are far from the number.<br> Better luck next time</div>";
             }
         }
     }
