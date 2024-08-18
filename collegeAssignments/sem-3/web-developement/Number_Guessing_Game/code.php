6aea916d6d73bacb67140c974c39dca8a2d88939
5wapnilmore <mswapnil383@gmail.com>
2024-08-18 18:04:37 +0530

    only design remaining

--- collegeAssignments/sem-3/web-developement/Number_Guessing_Game/code.php
+++ collegeAssignments/sem-3/web-developement/Number_Guessing_Game/code.php
@@ -1,27 +1,31 @@
-<?php 
+<?php
 
 $uNum = (int) $_POST['uNum'];
 $ranNum = (int) rand(0, 100);
 
-if (isset($uNum,$ranNum))
-{
-    if ($uNum > $ranNum) {
-        if ($uNum - $ranNum < 10) {
-            echo "You are within 10 of the number.";
-        } else if ($uNum - $ranNum < 20) {
-            echo "You are within 20 of the number.";
-        } else {
-            echo "You are far from the number.";
-        }
-    } else if ($uNum < $ranNum) {
-        if ($ranNum - $uNum < 10) {
-            echo "You are within 10 of the number.";
-        } else if ($ranNum - $uNum < 20) {
-            echo "You are within 20 of the number.";
-        } else {
-            echo "You are far from the number.";
+
+
+if (isset($uNum, $ranNum)) {
+    if ($uNum == $ranNum) {
+        echo "you entered $uNum and the random number was $ranNum<br>congrats you guessed the number.";
+    } else {
+        if ($uNum > $ranNum) {
+            if ($uNum - $ranNum < 10) {
+                echo "you entered $uNum and the random number was $ranNum<br>You are sooo close ! (within 10 of the number)";
+            } else if ($uNum - $ranNum < 20) {
+                echo "you entered $uNum and the random number was $ranNum<br>You are close (within 20 of the number.)";
+            } else {
+                echo "you entered $uNum and the random number was $ranNum<br>You are far from the number.";
+            }
+        } else if ($uNum < $ranNum) {
+            if ($ranNum - $uNum < 10) {
+                echo "you entered $uNum and the random number was $ranNum<br>You are sooo close ! (within 10 of the number)";
+            } else if ($ranNum - $uNum < 20) {
+                echo "you entered $uNum and the random number was $ranNum<br>You are close (within 20 of the number.)";
+            } else {
+                echo "you entered $uNum and the random number was $ranNum<br>You are far from the number.";
+            }
         }
     }
 }
-
 ?>
