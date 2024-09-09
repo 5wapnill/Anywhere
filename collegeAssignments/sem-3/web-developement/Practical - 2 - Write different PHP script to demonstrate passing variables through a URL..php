0d841e977b111a30ed045bd1fd886b2377fd9075
5wapnilmore <mswapnil383@gmail.com>
2024-09-09 22:39:45 +0530

    2nd practical

---
+++ collegeAssignments/sem-3/web-developement/Practical - 2 - Write different PHP script to demonstrate passing variables through a URL..php
@@ -0,0 +1,16 @@
+<?php
+    
+    $username = $_GET['username'];
+    $mobile = $_GET['mobile'];
+    $email = $_GET['email'];
+    $uname = $_GET['uname'];
+    $pass = $_GET['pass'];
+    
+    echo "Username :".$username."<br>";
+    echo "Mobile :".$mobile."<br>";
+    echo "Email Id :".$email."<br>";
+    echo "Username :".$uname."<br>";
+    echo "Password :".$pass."<br>";
+
+    //URL for this code : http://localhost/next.php?username=swapnil&mobile=1234567890&email=swapnilmore@gmail.com&uname=swapnil&pass=1234567890
+?>
