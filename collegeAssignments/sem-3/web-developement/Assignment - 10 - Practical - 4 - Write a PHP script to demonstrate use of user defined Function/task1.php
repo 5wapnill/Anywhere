9853c09cc6ec1858a0305db1fcbaf11a4415451b
5wapnilmore <mswapnil383@gmail.com>
2024-09-20 17:51:26 +0530

    completed assignment first task

---
+++ collegeAssignments/sem-3/web-developement/Assignment - 10 - Practical - 4 - Write a PHP script to demonstrate use of user defined Function/task1.php
@@ -0,0 +1,14 @@
+<?php
+
+function add($num1, $num2){
+    return $num1 + $num2;
+}
+
+$num1 = 5;
+$num2 = 3;
+
+$result = add($num1, $num2);
+
+echo "the addition of $num1 and $num2 is: $result";
+
+?>
